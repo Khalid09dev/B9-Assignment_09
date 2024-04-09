f4ccf127996a690e384dd7da9fc01bd70fccbbce
khalid09dev <mdkhalidhossen@hotmail.com>
2024-04-09 12:07:56 +0600

    installed swiper and initialize it with project

--- src/components/Root/Root.jsx
+++ src/components/Root/Root.jsx
@@ -1,9 +1,11 @@
 import Navbar from "../Navbar/Navbar";
+import {Outlet} from 'react-router-dom'
 
 const Root = () => {
     return (
         <div className="bg-gray-100">
             <Navbar></Navbar>
+            <Outlet></Outlet>
         </div>
     );
 };
