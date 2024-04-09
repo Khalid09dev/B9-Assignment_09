237302fb9071a21b04e75bd18a783d641c20f10e
khalid09dev <mdkhalidhossen@hotmail.com>
2024-04-09 22:55:33 +0600

    added a simple latest news section

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,6 +1,7 @@
 import AboutUs from "../AboutUs/AboutUs";
 import Banner from "../Banner/Banner";
 import Estates from "../Estates/Estates";
+import LatestNews from "../LatestNews/LatestNews";
 
 const Home = () => {
     return (
@@ -8,6 +9,7 @@ const Home = () => {
             <Banner></Banner>
             <Estates></Estates>
             <AboutUs></AboutUs>
+            <LatestNews></LatestNews>
         </div>
     );
 };
---
+++ src/components/LatestNews/News/News.jsx
@@ -0,0 +1,16 @@
+
+const News = ({news}) => {
+    const {id, date, category, title, image} = news;
+
+    return (
+        <div>
+            <div className="relative">
+                <img className="w-[325px] h-[370px] rounded-3xl" src={image} alt="#" />
+                <p className="absolute bottom-28 ml-16 text-[#FBA968] text-center  text-[16px] outfit font-medium ">{date} - {category}</p>
+                <p className="absolute bottom-7 text-[#FFFFFF] text-[24px] anek font-semibold text-center">{title}</p>
+            </div>
+        </div>
+    );
+};
+
+export default News;
