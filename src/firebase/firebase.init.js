724bd5cafa911861dab837d8b7d0e0ca4f9766cc
khalid09dev <mdkhalidhossen@hotmail.com>
2024-04-13 11:29:57 +0600

    added env-variables for firebase configuration

--- src/firebase/firebase.init.js
+++ src/firebase/firebase.init.js
@@ -8,13 +8,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyDYKKcldY8hcCZV2g33hjTnGHdyrymD7Vg",
-    authDomain: "project-zavar.firebaseapp.com",
-    projectId: "project-zavar",
-    storageBucket: "project-zavar.appspot.com",
-    messagingSenderId: "638282080751",
-    appId: "1:638282080751:web:5453e348ac5cc0af3c561a",
-    measurementId: "G-2LFQF73YV4"
+    apiKey: import.meta.env.VITE_APIKEY,
+    authDomain: import.meta.env.VITE_AUTHDOMAIN,
+    projectId: import.meta.env.VITE_PROJECTID,
+    storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+    messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+    appId: import.meta.env.VITE_APPID,
+    measurementId: import.meta.env.VITE_MEASUREMENTID
 };
 
 // Initialize Firebase
